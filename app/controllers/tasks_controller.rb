066d9401c8d31de9f5fd6fe35933902b6e03bdb7
enpen <baobaboo0319ch@gmail.com>
2020-02-15 07:18:41 +0000

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :authenticate_user, only:[:show, :create, :edit, :update, :destroy]
+  before_action :authenticate_user, only:[:show, :edit, :update, :destroy]
   
   include SessionsHelper
   
